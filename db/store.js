4093937e69df53b2ad1bc498417fef50366cc5e4
Sam Lerner <sam.a.lerner@gmail.com>
2023-01-04 15:18:06 -0500

    notes added to reinforce function

--- db/store.js
+++ db/store.js
@@ -2,16 +2,21 @@ const uuid = require('../helpers/uuid');
 const fs = require('fs');
 const util = require('util');
 
+// fs functions 
 const readFile = util.promisify(fs.readFile);
 const writeFile = util.promisify(fs.writeFile);
 
+// Class constructor for all of our functions
 class Store {
+    // Read from the stored json
     read() {
         return readFile('db/db.json', 'utf8');
     }
+    // Write to the stored json
     write(note) {
         return writeFile('db/db.json', JSON.stringify(note))
     }
+    // Create a new note to store
     addNote(note) {
         const { title, text } = note
 
@@ -26,12 +31,14 @@ class Store {
             .then(updatedNotes => this.write(updatedNotes))
             .then(() => this.newNote)
     }
+    // Get notes (on page load and on store)
     getNotes() {
         return this.read()
             .then(notes => {
                 return JSON.parse(notes) || [];
             })
     }
+    // Delete a note
     removeNote(id) {
         return this.getNotes()
             .then(notes => notes.filter(note => note.id !== id))
